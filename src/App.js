736d89415aba8af5fb7b6bd24d6139d866b67511
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-05-09 17:37:20 +0000

    React class walkthrough

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
+// import react into your project
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// call name of file and Component to make a state
+class App extends React.Component {
+  // state defines properties of the react object, serves as a trigger for events in react
+  state = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    submitted: false,
+  }
+  
+  handleSubmit = () => {
+    this.setState({
+      submitted: true,
+    })
+  }
+  // how to create a method 
+  setName = event => {
+    console.log(event.target.value)
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  setEmail = event => {
+    console.log(event.target.value)
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  setNumber = event => {
+    console.log(event.target.value)
+    this.setState({
+      phoneNumber: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h2>Social Profile</h2>
+      <p>An app for your social media profiles</p>
+      {!this.state.submitted &&
+      <div>
+      <input onChange={this.setName} style={{"display": "block"}} placeholder="Name"/>
+      <input onChange={this.setEmail} style={{"display": "block"}} placeholder="email"/>
+      <input onChange={this.setNumber} style={{"display": "block"}} placeholder="phone number"/>
+      <button onClick={this.handleSubmit}>Submit</button>
+      </div>}
+     
+       { this.state.submitted && <div>
+      {"Your Name is: " + this.state.name}
+      <br/>
+      {"Your E-mail is: " + this.state.email}
+      <br/>
+      {"Your Phone Number is: " + this.state.phoneNumber}
+       </div>}
+       
+       </div>
+    );
+  }
+
 }
 
 export default App;
